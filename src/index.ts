e0e9c1a4916b06c1253a1377950f24725c72069d
Mauricio Ferreira <mauriciovictor17@gmail.com>
2022-05-24 10:56:07 -0300

    add documentation with swagger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
 
+import swaggerUi from "swagger-ui-express";
 import { usersRoutes } from "./routes/users.routes";
+
+// eslint-disable-next-line import/no-unresolved
+import swaggerConfig from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 app.use("/users", usersRoutes);
 
 export { app };
